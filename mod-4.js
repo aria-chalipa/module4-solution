3b56a70ed009eea9c478d935b67f7eeef987c24a
aria-chalipa <104375858+aria-chalipa@users.noreply.github.com>
2022-05-21 21:12:13 +0430

    Add files via upload

---
+++ mod-4.js
@@ -0,0 +1,16 @@
+var text = ["aria","json", "jon","reza"];
+var z;
+for (i=0; i<text.length; i++){
+     z = text[i];
+     var a =z.charAt(0);
+     
+
+
+
+if (a=="j" || a=="J"){
+    console.log("goodby "+ z)
+}
+else{
+    console.log("hi "+ z)
+}
+}
